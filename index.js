35bc45d69e87e5e2efda6f18c54c5c8ee31d327e
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-06-07 15:32:00 -0400

    Updated gallery, added more pictures

--- index.js
+++ index.js
@@ -41,5 +41,5 @@ function showSlides() {
     slideIndex = 1;
   }
   slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 3500); // Change image every 2 seconds
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
